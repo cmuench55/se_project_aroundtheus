cbe77f01213f928885c0b6106522c69f933aed64
Connor.Muench <cmuench55@gmail.com>
2024-09-02 20:36:11 -0500

    fixed commas

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-        title: "Yosemite Valley",
+        title: "Yosemite Valley"
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
     },
     {
-        title: "Lake Louise",
+        title: "Lake Louise"
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
     },
     {
-        title: "Bald Mountains",
+        title: "Bald Mountains"
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
     },
     {
-        title: "Latemar",
+        title: "Latemar"
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
     },
     {
-        title: "Vanoise National Park",
+        title: "Vanoise National Park"
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
     },
     {
-        title: "Lago di Braies",
+        title: "Lago di Braies"
         link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
-    },
+    }
 ];
