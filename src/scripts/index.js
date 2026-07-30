dfa24bc8f0f73aa3a8ccdc20e0c5e6636028a628
cmuench <cmuench55@gmail.com>
2026-07-29 20:30:21 -0500

    Update API token for provided credentials

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -25,7 +25,7 @@ const userInfo = new UserInfo({
 const api = new Api({
   baseUrl: "https://around-api.en.tripleten-services.com/v1",
   headers: {
-    authorization: "c56e30dc-2883-4270-a59e-b2f7bae969c6",
+    authorization: "1b3cac0a-3105-41b5-b45b-c70fe76e4355",
     "Content-Type": "application/json",
   },
 });
